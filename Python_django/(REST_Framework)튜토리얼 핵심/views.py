c368bdb615c705ac775801998f7e92c39682ee13
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-09-07 09:32:07 -0500

    Update views.py

--- Python_django/(REST_Framework)튜토리얼 핵심/views.py
+++ Python_django/(REST_Framework)튜토리얼 핵심/views.py
@@ -13,195 +13,195 @@ from rest_framework.permissions import IsAuthenticated
 
 ####### 일반적인 장고 FBV ################################################################################
 
-# @csrf_exempt
-# def article_list(request):
-#     if request.method == 'GET':
-#         articles = Article.objects.all()
-#         serializer = ArticleSerializer(articles, many=True)
-#         return JsonResponse(serializer.data, safe = False)
+@csrf_exempt
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe = False)
     
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = ArticleSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
-# @csrf_exempt
-# def article_detail(request, pk):
-#     try:
-#         article = Article.objects.get(pk=pk)
-#     except Article.DoesNotExist:
-#         return HttpResponse(status=404)
+@csrf_exempt
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
     
-#     if request.method == 'GET':
-#         serializer = ArticleSerializer(article)
-#         return JsonResponse(serializer.data)
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
 
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = ArticleSerializer(article, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
 
-#     elif request.method == 'DELETE':
-#         article.delete()
-#         return HttpResponse(status=400)
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=400)
 
 ####### Rest_framework api_view 장식자(FBV) ################# with authentication, permission ########
 
-# @api_view(['GET','POST'])
-# # @authentication_classes([SessionAuthentication, BasicAuthentication]) # 일반 세션 인증과 사용자인증
-# # @authentication_classes([TokenAuthentication]) # 토큰을 이용한 인증
-# # @permission_classes([IsAuthenticated])    # 인증이 되었는지
-# def article_list(request):
-#     if request.method == 'GET':
-#         articles = Article.objects.all()
-#         serializer = ArticleSerializer(articles, many=True)
-#         return Response(serializer.data)
+@api_view(['GET','POST'])
+# @authentication_classes([SessionAuthentication, BasicAuthentication]) # 일반 세션 인증과 사용자인증
+# @authentication_classes([TokenAuthentication]) # 토큰을 이용한 인증
+# @permission_classes([IsAuthenticated])    # 인증이 되었는지
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
     
-#     elif request.method == 'POST':
-#         serializer = ArticleSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# @api_view(['GET','PUT','DELETE'])
-# # @authentication_classes([SessionAuthentication, BasicAuthentication])
-# # @authentication_classes([TokenAuthentication])
-# # @permission_classes([IsAuthenticated])
-# def article_detail(request, pk):
-#     try:
-#         article = Article.objects.get(pk=pk)
-#     except Article.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
+@api_view(['GET','PUT','DELETE'])
+# @authentication_classes([SessionAuthentication, BasicAuthentication])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
-#     if request.method == 'GET':
-#         serializer = ArticleSerializer(article)
-#         return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
 
-#     elif request.method == 'PUT':
-#         serializer = ArticleSerializer(article, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     elif request.method == 'DELETE':
-#         article.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 ####### Rest_framework APIView 뷰(CBV) ####################################### 함수형과 매우 비슷함 ##########
 
-# class ArticleAPIview(APIView) :
-#     def get(self, request):
-#         articles = Article.objects.all()
-#         serializer = ArticleSerializer(articles, many=True)
-#         return Response(serializer.data)
+class ArticleAPIview(APIView) :
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
 
-#     def post(self, request):
-#         serializer = ArticleSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# class ArticleDetails(APIView) :
-#     def get_object(self, id):
-#         try:
-#             return Article.objects.get(id=id)
-#         except Article.DoesNotExist:
-#             return Response(status=status.HTTP_404_NOT_FOUND)
+class ArticleDetails(APIView) :
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
     
-#     def get(self, request, id):
-#         article = self.get_object(id)
-#         serializer = ArticleSerializer(article)
-#         return Response(serializer.data)
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
 
-#     def put(self, request, id):
-#         article = self.get_object(id)
-#         serializer = ArticleSerializer(article, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def delete(self, request, id):
-#         article = self.get_object(id)
-#         article.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 ####### Rest_framework Generic 뷰(CBV) & Mixins ################# with authentication, permission ###########
 
-# class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin,
-# mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-#     serializer_class = ArticleSerializer
-#     queryset = Article.objects.all()
-#     lookup_field = 'id'
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'
 
-#     # authentication_classes = [SessionAuthentication, BasicAuthentication] # 일반 세션 인증과 사용자인증
-#     # authentication_classes = [TokenAuthentication]  # 토큰을 이용한 인증(settings apps에 입력해야함)
-#     # permission_classes = [IsAuthenticated]
+    # authentication_classes = [SessionAuthentication, BasicAuthentication] # 일반 세션 인증과 사용자인증
+    # authentication_classes = [TokenAuthentication]  # 토큰을 이용한 인증(settings apps에 입력해야함)
+    # permission_classes = [IsAuthenticated]
     
-#     def get(self, request, id=None):
-#         if not id:
-#             return self.list(request)
-#         else:
-#             return self.retrieve(request)
+    def get(self, request, id=None):
+        if not id:
+            return self.list(request)
+        else:
+            return self.retrieve(request)
     
-#     def post(self, request):
-#         return  self.create(request)
+    def post(self, request):
+        return  self.create(request)
             
-#     def put(self, request, id=None):
-#         return  self.update(request, id)
+    def put(self, request, id=None):
+        return  self.update(request, id)
 
-#     def delete(self, request, id):
-#         return  self.destroy(request, id)
+    def delete(self, request, id):
+        return  self.destroy(request, id)
 
 ####### Rest_framework APIView (CBV) ViewSet ########################################################
 
-# class ArticleViewSet(viewsets.ViewSet):
-#     def list(self, request):
-#         articles = Article.objects.all()
-#         serializer = ArticleSerializer(articles, many=True)
-#         return Response(serializer.data)
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
 
-#     def create(self, request):
-#         serializer = ArticleSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def retrieve(self, request, pk=None):
-#         queryset = Article.objects.all()
-#         article = get_object_or_404(queryset, pk=pk)
-#         serializer = ArticleSerializer(article)
-#         return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
 
-#     def update(self, request, pk=None):
-#         article = Article.objects.get(pk=pk)
-#         serializer = ArticleSerializer(article, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def delete(self, request, pk=None):
-#         article = Article.objects.get(pk=pk)
-#         article.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 ####### Rest_framework Generic 뷰(CBV) ViewSet ########################################################
 
-# class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin,
-# mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-#     serializer_class = ArticleSerializer
-#     queryset = Article.objects.all()
+class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
 ####### Rest_framework model ViewSet ########################################################
 
