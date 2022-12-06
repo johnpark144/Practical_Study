1e7d6ac05b304f51dc587a60b0ac17274b4882a6
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-12-06 08:45:58 -0600

    Update React_practice.js

--- React_practice.js
+++ React_practice.js
@@ -757,3 +757,56 @@ export default function CreateWord() {
         </form>
     )
 }
+
+// #### UseReducer ###############################################################################################################################
+// #### 은행 : input에 액수(number)를 입력한뒤 + or -를 누르면 계산됨(money) ########################################################################
+import { useReducer, useState } from "react";
+
+// reducer - state를 업데이트 하는 역할 (은행)
+// dispatch - state 업데이트를 위한 요구
+// action - 요구의 내용
+
+const ACTION_TYPES = {
+  deposit: 'deposit',
+  withdraw: 'withdraw',
+}
+
+const reducer = (state, action) => {
+  switch (action.type){
+    case ACTION_TYPES.deposit:
+      return state + action.payload;
+    case ACTION_TYPES.withdraw:
+      return state - action.payload;
+    default:
+      return state
+  }
+};
+
+function App(){
+  const [number, setNumber] = useState(0);
+  const [money, dispatch] = useReducer(reducer, 0); // const [state, action] = useReducer(reducer함수, state초기값)
+  return(
+    <div>
+      <p>잔고: {money}원</p>
+
+      <input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))} step="1000"/>
+      
+      <button onClick={()=>{
+        dispatch({ type:ACTION_TYPES.deposit, payload: number })    // dispatch({ action.type, action.payload })
+      }}>
+        +
+      </button>
+
+      <button onClick={()=>{
+        dispatch({ type:ACTION_TYPES.withdraw, payload: number })
+      }}>
+        -
+      </button>
+    </div>
+  )
+}
+
+export default App;
+
+// #### UseReducer ###############################################################################################################################
+// #### 은행 : input에 액수(number)를 입력한뒤 + or -를 누르면 계산됨(money) ########################################################################
