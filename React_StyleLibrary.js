0d2b8a8417d2568c01b6e032851a917d7f916af8
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-12-08 12:52:31 -0600

    Update React_StyleLibrary.js

--- React_StyleLibrary.js
+++ React_StyleLibrary.js
@@ -1,1 +1,9 @@
- 
+// ######## React-icons #################################################################################################################
+// npm install react-icons --save
+
+import { AiFillEdit } from 'react-icons/ai'; // Ai로 시작하기 때문에 from 'react-icons/ai'
+import { MdDone } from 'react-icons/md';
+
+<AiFillEdit /> // React-icons 사이트에서 복사한 컴포넌트
+<AiFillDelete />
+<MdDone />
