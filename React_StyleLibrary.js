f152ad9b500b42823413f1845dcacfab12bad2b2
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-12-08 18:29:21 -0600

    Update React_StyleLibrary.js

--- React_StyleLibrary.js
+++ React_StyleLibrary.js
@@ -1,4 +1,5 @@
 // ######## React-icons #################################################################################################################
+// https://react-icons.github.io/react-icons/
 // npm install react-icons --save
 
 import { AiFillEdit } from 'react-icons/ai'; // Ai로 시작하기 때문에 from 'react-icons/ai'
@@ -7,3 +8,14 @@ import { MdDone } from 'react-icons/md';
 <AiFillEdit /> // React-icons 사이트에서 복사한 컴포넌트
 <AiFillDelete />
 <MdDone />
+
+// ######## React-beautiful-dnd #################################################################################################################
+// https://www.npmjs.com/package/react-beautiful-dnd
+// npm i react-beautiful-dnd
+// npm i @types/react-beautiful-dnd
+  
+import { DragDropContext } from 'react-beautiful-dnd'
+
+<DragDropContext>
+  
+</DragDropContext>
