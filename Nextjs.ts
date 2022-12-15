e76386b8d69435ee8ac41e3a0763ca9fb6e4a13d
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-12-15 12:47:48 -0600

    Update Nextjs.ts

--- Nextjs.ts
+++ Nextjs.ts
@@ -553,6 +553,19 @@ function LoggoutButton() {
 
 export default LoggoutButton;
 
+// ######## Head, title ##########################################################################################################################
+// ######## head.tsx  // nextjs13에선 Head가 예약어로 되있어서 head역할하게함
+export default function Head() {
+  return (
+    <>
+      <title>Meta Messenger</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
+
+
 // ######## ChatInput 컴포넌트 with tailwind Css ########################################################################################################
 // ############ page.tsx
 import React from 'react'
