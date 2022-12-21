e33183f786e10148cbf91deedd4913adf431d4c2
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-12-21 08:41:34 -0600

    Update Nextjs.ts

--- Nextjs.ts
+++ Nextjs.ts
@@ -17,12 +17,13 @@
 // https://blog.theashishmaurya.me/using-tailwind-css-in-nextjs-13-with-turbopack // 터보팩 tailwindcss 참고
 
 // npx create-next-app --example with-turbopack // 터보팩
-// npx add2next tailwind --turbo // tailwindcss
+// npm i -- save-dev concurrently // 
+
 // npm run dev --turbo // 터보팩 서버
 
 // ######### package.json
-// "dev": "concurrently \"next dev\" \"tailwindcss --input ./app/styles/globals.css --output ./app/styles/dist.css --watch\"",
-// "build": "tailwindcss ./app/styles/globals.css --output ./app/styles/dist.css && next build",
+// "dev": "concurrently \"next dev --turbo\" \"tailwindcss --input styles/globals.css --output styles/dist.css --watch\"",
+// "build": "tailwindcss -i styles/globals.css --output styles/dist.css && next build",
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // @@@@@@ 투두리스트API, 구글서치API (nextjs13) @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
