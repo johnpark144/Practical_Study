fc265363f85677770e1a3cc34fc82f77b85c2140
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-01-20 16:34:17 -0600

    Create _Deploy.js

---
+++ _Deploy.js
@@ -0,0 +1,12 @@
+// ########### vercel #########################################################################################################################
+// https://vercel.com/
+// npm i -g vercel
+
+// 그이후 터미널에서 vercel 타입
+// ? Set up and deploy “C:\coding\doortodoor”? [Y/n] y
+// ? Which scope do you want to deploy to? johnpark144
+// ? Link to existing project? [y/N] n
+// ? What’s your project’s name? doortodoor
+// ? In which directory is your code located? ./
+
+// ###########  #########################################################################################################################
