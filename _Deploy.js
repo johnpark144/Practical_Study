268c87d9fe70ca2dab1211ba5b31339e85017ebd
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-01-20 17:26:26 -0600

    Update _Deploy.js

--- _Deploy.js
+++ _Deploy.js
@@ -4,7 +4,7 @@
 // https://vercel.com/
 // npm i -g vercel
 
-// 그이후 터미널에서 vercel 타입
+// 그이후 터미널에서 vercel 입력
 // ? Set up and deploy “C:\coding\doortodoor”? [Y/n] y
 // ? Which scope do you want to deploy to? johnpark144
 // ? Link to existing project? [y/N] n
@@ -13,5 +13,6 @@
 
 // Settings -> Environment Variables -> .env복사 -> pase
 
+// vercel --prod // 재배치
 
 // ###########  #########################################################################################################################
