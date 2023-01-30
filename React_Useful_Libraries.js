5f02413cc2855984448b5bdb57bf25b5a4df2529
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-01-30 11:16:31 -0600

    Update React_Useful_Libraries.js

--- React_Useful_Libraries.js
+++ React_Useful_Libraries.js
@@ -12,7 +12,7 @@ plugins: [
     require('tailwind-scrollbar'),
 ],
 // ########
-    <div className='scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 '>
+    <div className='scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 '>
 
 // ######## react-hook-form (리액트 훅 form) #########################################################################################
 // npm i react-hook-form
