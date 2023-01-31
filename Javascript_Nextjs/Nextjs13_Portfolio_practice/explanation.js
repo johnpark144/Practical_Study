e508ca0ee2246590968d12c0e704127ca635502f
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-01-31 10:25:45 -0600

    Update and rename zz to explanation.js

---
+++ Javascript_Nextjs/Nextjs13_Portfolio_practice/explanation.js
@@ -0,0 +1,3 @@
+Pizza -> Framer Motion, Regular Css
+Portfolio1 -> GreenSock(GSAP), Animate.CSS, Loader, EmailJS, Scss // SVG 애니메이션 생성, 애니메이션 문자하나씩, 큐브 애니메이션, 로더, 이메일, 지도
+Portfolio2 -> typewriter, react hook form, react-scroll-horizontal, react-lottie-player,  tailwind-scrollbar, tailwind // 타입라이터, 리액트 훅 폼, 수평마우스스크롤, 로티플레이어, 테일윈드용 스크롤색
