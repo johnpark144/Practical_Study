85bb4315174059b5bb2cb4824d6d386898f05288
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-02-01 15:05:45 -0600

    Update React_Styled-components_ThreeJS_GSAP.js

--- React_Styled-components_ThreeJS_GSAP.js
+++ React_Styled-components_ThreeJS_GSAP.js
@@ -552,4 +552,118 @@ export default function BatterySection() {
   )
 }
 
+// ################ ColorSection.jsx
+import React, { useRef, useLayoutEffect, Suspense } from "react";
+import styled from "styled-components";
+import gsap from "gsap";
+import { useGLTF } from '@react-three/drei';
+import { Model2 } from './../../public/Apple_iphone_13_pro_max2';
+import { Canvas } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+
+// ... 생략 ...
+
+export default function ColorSection() {
+  const sectionRef = useRef(null);
+  const rightRef = useRef(null);
+  const leftRef = useRef(null);
+  const textRef = useRef(null);
+  const { materials } = useGLTF('/apple_iphone_13_pro_max.glb');
+
+  useLayoutEffect(() => {
+    let Elem = sectionRef.current;
+    let rightElem = rightRef.current;
+    let leftElem = leftRef.current;
+    let textElem = textRef.current;
+    
+    // 스크롤시 정보 변경할 함수 
+    let updateColor = (color, text, rgbColor) => {
+      materials.Body.color.set(color);
+
+      textElem.innerText = text;
+      textElem.style.color = color;
+      rightElem.style.backgroundColor = `rgba(${rgbColor}, 0.4)`;
+      leftElem.style.backgroundColor = `rgba(${rgbColor}, 0.8)`;
+    };
+
+    gsap.to(Elem, {
+      scrollTrigger: {
+        trigger: Elem,
+        start: "top top",
+        end: `+=${Elem.offsetWidth + 1000}`,
+        scrub: 1,
+        pin: true,    // 고정
+        pinSpacing: true,
+      },
+    });
+
+    let t2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: Elem,
+          start: "top top",
+          end: `+=${Elem.offsetWidth + 1000}`,
+          scrub: 1,
+        },
+      })
+      .to(Elem, {
+        onStart: updateColor,   // 시작시 작동할 함수
+        onStartParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"], // 함수 Props
+        onReverseComplete: updateColor, // 스크롤 다시 돌아갈때 작동할 함수
+        onReverseCompleteParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"], // 함수 Props
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#F9E5C9", "Gold", "249, 229, 201"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#F9E5C9", "Gold", "249, 229, 201"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#A50011", "Red", "165, 0, 17"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#A50011", "Red", "165, 0, 17"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#215E7C", "Blue", "33, 94, 124"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#215E7C", "Blue", "33, 94, 124"],
+      });
+
+      return () => {
+        if (t2) t2.kill();
+      };
+  }, []);
+
+  return (
+    <Section ref={sectionRef}>
+      <Left ref={leftRef} />
+      <Center ref={textRef} />
+      <Right ref={rightRef}>
+        <Canvas camera={{ fov: 6.5 }}>
+          <ambientLight intensity={1.25} />
+          <directionalLight intensity={0.4} />
+          <Suspense fallback={null}>
+            <Model2 />
+          </Suspense>
+          <Environment preset="night" />
+        </Canvas>
+      </Right>
+    </Section>
+  );
+}
+
+
 // #########################################################################################################################################################
