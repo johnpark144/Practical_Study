06e39fdc13a2f60df059d57eae1f7299631e713c
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-02-02 14:39:54 -0600

    Update React_Styled-components_ThreeJS_GSAP.js

--- React_Styled-components_ThreeJS_GSAP.js
+++ React_Styled-components_ThreeJS_GSAP.js
@@ -778,5 +778,73 @@ export default function PricingSection() {
 }
 
 
-// ########  #########################################################################################################
+// ######## GSAP (matchMedia, ) #########################################################################################################
+// https://greensock.com/docs/v3/GSAP/gsap.matchMedia()
+
+// ################ Model.jsx
+import React, { useRef, useLayoutEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import gsap from "gsap";
+import { useThree } from "@react-three/fiber";
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/apple_iphone_13_pro_max.glb");
+
+  const group = useRef();
+  let camera = useThree((state) => state.camera);
+  let scene = useThree((state) => state.scene);
+
+  useLayoutEffect(() => {
+    camera.position.set(0, 2, 6);
+    materials.Body.color.set("#9BB5CE"); 
+
+    let fov = camera.fov;  // field of view (zoom)
+    fov = (1400 * 18) / window.innerWidth;
+    camera.fov = fov;
+    camera.updateProjectionMatrix();
+
+    let mm = gsap.matchMedia(); // matchMedia로 반응형으로 적용하게 가능
+    mm.add(
+      {
+        isDesktop: `(min-width: 48em)`,
+        isMobile: `(max-width:48em`,
+      },
+      (context) => {
+        let { isDesktop, isMobile } = context.conditions;
+
+        let t1 = gsap.timeline({
+          scrollTrigger: {
+            trigger: "#phone-model",
+            start: "top+=100 top",
+            endTrigger: "#battery",
+            end: "top top",
+            scrub: true,
+          },
+        });
+
+        t1.fromTo(camera.position, { y: 2 }, { y: 0 })
+          .to(scene.rotation, { y: 0.8 })
+          .to(scene.rotation, { y: 3 })
+          .to(scene.rotation, { z: 1.58 }, "key1")
+          .to(camera.position, { z: 4 }, "key1")
+          .to(scene.rotation, { y: 0, z: 0 }, "key2")
+          .to(camera.position, { z: 6, x: isDesktop ? -1 : 0 }, "key2") // 이와같이 matchMedia로 반응형 적용가능
+          .to(scene.rotation, { z: 0, y: 6.3 }, "key3")
+          .to(camera.position, { x: isDesktop ? 0.8 : 0, y: 0 }, "key3");
+
+        if (isMobile) {
+          camera.fov = 20;
+          camera.updateProjectionMatrix();
+        }
+
+        return () => {
+          if (t1) t1.kill();
+        };
+      }
+    );
+  }, []);
+
+  return (
+// ... 생략 ...
+
 
