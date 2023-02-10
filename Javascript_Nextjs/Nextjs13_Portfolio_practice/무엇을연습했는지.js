d779f00f3d9b3364d8f552eec8dda264b571a758
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-02-10 08:51:33 -0600

    Update 무엇을연습했는지.js

--- Javascript_Nextjs/Nextjs13_Portfolio_practice/무엇을연습했는지.js
+++ Javascript_Nextjs/Nextjs13_Portfolio_practice/무엇을연습했는지.js
@@ -1,3 +1,7 @@
+// -- 기본페이지 // 스크롤 인 아웃 될때 효과, 스크롤 스피드 다르게, 마우스에 따른 SVG
+-> scroll-out, react-rellax, Framer Motion
+  
+//  Framer Motion (기본, AnimatePresence(useState,useLocation 등으로 인하여 사라질때 애니메이션), 반복적인 두 로더, 아이템드래그, useState없이 현재 스크롤 위치 )
 Pizza -> Framer Motion, Regular Css
 
 // SVG 애니메이션 생성, 애니메이션 문자하나씩, 큐브 애니메이션, 로더, 이메일, 지도
