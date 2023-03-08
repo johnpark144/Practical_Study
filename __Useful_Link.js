52faf6e8a6850657bfcc8c29118fef1c9a2df8f1
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-03-08 17:12:38 -0600

    Update __Useful_Link.js

--- __Useful_Link.js
+++ __Useful_Link.js
@@ -1,51 +1,37 @@
 ######## 북마크 ################################################################################################################################
-https://pixlr.com/kr/e/#home
-https://www.figma.com/files/recent?fuid=1166465294877234719
-
-Css, 애니메이션.
-)S와 라이브러리
-localhost
-파이션
-백연드, Ap 및배포
-기타 개발관련
-개발 참고 ) mygithub
-Online Sermon...
-Papago
-Zoom
-Along with the Go..
-프토샵
-피그마 (드구
-Dribbble I 월사이트 컨셉 참고
-pixabay| 각종 이미지 모음
-d Freepik I 각종 이미지 모음2
-Unsplash I 각종 이미지 모음3
-Halikei I 기하학적 배경화면
-Shape Divider App I 기하학적 배경화면2
-O cool Backgrounds I 심풀한 배경이미지
-OPNGEaqIPNG 파일 모움
-1 구글 머티리얼 아이폰
-B부트스트랩
-N 폰트어썸아이콘 1
-F폰트어썸아이큰 2
-Tailwind CSS | Docs
-Talilwind Css Alerts | Tailwind 컴포년트 모음
-Tailwind U KIT | Talwind 컴포넌트 모음2
-Flowbite | Tailwind 컵포년트 모음3
-Framer Motion I 애니메이션 라이브러리
-수 Animate.cs I 애니메이션 효과 모음 라이브러리
-Animista I 애니메이선 효과 모읍
-Hover.css I 하버효과 모음
-표 css Grid Generator| 그리드 주풀
-css Gradient I 그라데이션 효과 주출
-colorspace I 어울리는 컬러
-My Dashboard I 각종 Converter 및 svG 만들기
-Greensock (GSAD) I 얘니메이션 라이브러리
-LottieFles I 다양한 GIF 그림 다운
-sketchfab I 다양한 3 파일 다운
-:: Creators 3D I3D 컨버터
-Metaverse 3D Avatar Creator 아바타 만들기
-Free profile picture Maker I 프로필 사진 배경 만들기
-cubic-bezier I transition 속력 확인
-버튼 디자인 스타일 모음
-Loaders, Spinners 모음
-: Css 매니메이션 18가지 예제
+프토샵 https://pixlr.com/kr/e/#home
+피그마 https://www.figma.com/files/recent?fuid=1166465294877234719
+Dribbble | 월사이트 컨셉 참고 https://dribbble.com/
+pixabay | 각종 이미지 모음 https://pixabay.com/
+Freepik | 각종 이미지 모음2 https://www.freepik.com/
+Unsplash | 각종 이미지 모음3 https://unsplash.com/
+Halikei | 기하학적 배경화면 https://app.haikei.app/
+Shape Divider App | 기하학적 배경화면2 https://www.shapedivider.app/
+cool Backgrounds | 심풀한 배경이미지 https://coolbackgrounds.io/
+PNGEgg | PNG 파일 모움 https://www.pngegg.com/
+구글 머티리얼 아이폰 https://fonts.google.com/icons
+부트스트랩 https://getbootstrap.kr/docs/5.1/getting-started/introduction/
+폰트어썸아이콘 1 https://fontawesome.com/icons
+폰트어썸아이큰 2 https://fontawesomeicons.com/send-o
+Tailwind CSS | Docs https://tailwindcss.com/docs/installation
+Talilwind Css Alerts | Tailwind 컴포년트 모음 https://tailwindcomponents.com/components/alerts
+Tailwind U KIT | Talwind 컴포넌트 모음2 https://www.tailwind-kit.com/components
+Flowbite | Tailwind 컵포년트 모음3 https://flowbite.com/docs/getting-started/introduction/#figma
+Framer Motion | 애니메이션 라이브러리 https://www.framer.com/motion/
+Animate.css | 애니메이션 효과 모음 라이브러리 https://animate.style/
+Animista | 애니메이선 효과 모읍 https://animista.net/play/text/tracking-in
+Hover.css | 하버효과 모음 http://ianlunn.github.io/Hover/
+css Grid Generator| 그리드 추출 https://cssgrid-generator.netlify.app/?fbclid=IwAR2xX6U6DfhzDHmjXMiyBeiYhXr9m094SKHrIOYHu3zghabCudQpb4cwSFc
+css Gradient | 그라데이션 효과 추출 https://cssgradient.io/
+colorspace | 어울리는 컬러 https://mycolor.space/
+My Dashboard | 각종 Converter 및 SVG 만들기 
+Greensock (GSAD) | 얘니메이션 라이브러리 https://www.fetoolkit.io/dashboard/MmFkLTIwNA
+LottieFles | 다양한 GIF 그림 다운 https://lottiefiles.com/
+sketchfab | 다양한 3D 파일 다운 https://sketchfab.com/search?type=models
+Creators 3D | 3D 컨버터 https://www.creators3d.com/online-viewer
+Metaverse 3D Avatar Creator | 아바타 만들기 https://readyplayer.me/
+Free profile picture Maker | 프로필 사진 배경 만들기 https://pfpmaker.com/
+cubic-bezier | transition 속력 확인 https://cubic-bezier.com/#.17,.67,.83,.67
+버튼 디자인 스타일 모음 https://inpa.tistory.com/entry/CSS-%F0%9F%92%8D-%EB%B2%84%ED%8A%BC-%EB%94%94%EC%9E%90%EC%9D%B8-%EB%AA%A8%EC%9D%8C
+Loaders, Spinners 모음 https://cssloaders.github.io/
+Css 매니메이션 18가지 예제 https://ldrerin.tistory.com/397
