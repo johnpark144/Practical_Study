cff0e1c0a30362afb0f689c570f2f3c514319996
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-03-08 17:32:53 -0600

    Update __Useful_Link.js

--- __Useful_Link.js
+++ __Useful_Link.js
@@ -1,52 +1,70 @@
 ######## 북마크 ################################################################################################################################
-프토샵 https://pixlr.com/kr/e/#home
-피그마 https://www.figma.com/files/recent?fuid=1166465294877234719
-Dribbble | 월사이트 컨셉 참고 https://dribbble.com/
-pixabay | 각종 이미지 모음 https://pixabay.com/
-Freepik | 각종 이미지 모음2 https://www.freepik.com/
-Unsplash | 각종 이미지 모음3 https://unsplash.com/
-Halikei | 기하학적 배경화면 https://app.haikei.app/
-Shape Divider App | 기하학적 배경화면2 https://www.shapedivider.app/
-cool Backgrounds | 심풀한 배경이미지 https://coolbackgrounds.io/
-PNGEgg | PNG 파일 모움 https://www.pngegg.com/
-구글 머티리얼 아이폰 https://fonts.google.com/icons
-부트스트랩 https://getbootstrap.kr/docs/5.1/getting-started/introduction/
-폰트어썸아이콘 1 https://fontawesome.com/icons
-폰트어썸아이큰 2 https://fontawesomeicons.com/send-o
-Tailwind CSS | Docs https://tailwindcss.com/docs/installation
-Talilwind Css Alerts | Tailwind 컴포년트 모음 https://tailwindcomponents.com/components/alerts
-Tailwind U KIT | Talwind 컴포넌트 모음2 https://www.tailwind-kit.com/components
-Flowbite | Tailwind 컵포년트 모음3 https://flowbite.com/docs/getting-started/introduction/#figma
-Framer Motion | 애니메이션 라이브러리 https://www.framer.com/motion/
-Animate.css | 애니메이션 효과 모음 라이브러리 https://animate.style/
-Animista | 애니메이선 효과 모읍 https://animista.net/play/text/tracking-in
-Hover.css | 하버효과 모음 http://ianlunn.github.io/Hover/
-css Grid Generator| 그리드 추출 https://cssgrid-generator.netlify.app/?fbclid=IwAR2xX6U6DfhzDHmjXMiyBeiYhXr9m094SKHrIOYHu3zghabCudQpb4cwSFc
-css Gradient | 그라데이션 효과 추출 https://cssgradient.io/
-colorspace | 어울리는 컬러 https://mycolor.space/
-My Dashboard | 각종 Converter 및 SVG 만들기 
-Greensock (GSAD) | 얘니메이션 라이브러리 https://www.fetoolkit.io/dashboard/MmFkLTIwNA
-LottieFles | 다양한 GIF 그림 다운 https://lottiefiles.com/
-sketchfab | 다양한 3D 파일 다운 https://sketchfab.com/search?type=models
-Creators 3D | 3D 컨버터 https://www.creators3d.com/online-viewer
-Metaverse 3D Avatar Creator | 아바타 만들기 https://readyplayer.me/
-Free profile picture Maker | 프로필 사진 배경 만들기 https://pfpmaker.com/
-cubic-bezier | transition 속력 확인 https://cubic-bezier.com/#.17,.67,.83,.67
-버튼 디자인 스타일 모음 https://inpa.tistory.com/entry/CSS-%F0%9F%92%8D-%EB%B2%84%ED%8A%BC-%EB%94%94%EC%9E%90%EC%9D%B8-%EB%AA%A8%EC%9D%8C
-Loaders, Spinners 모음 https://cssloaders.github.io/
-Css 매니메이션 18가지 예제 https://ldrerin.tistory.com/397
+######## CSS, 애니메이션, 이미지 관련
+프토샵                                              https://pixlr.com/kr/e/#home
+피그마                                              https://www.figma.com/files/recent?fuid=1166465294877234719
+Dribbble | 월사이트 컨셉 참고                        https://dribbble.com/
+pixabay | 각종 이미지 모음                           https://pixabay.com/
+Freepik | 각종 이미지 모음2                          https://www.freepik.com/
+Unsplash | 각종 이미지 모음3                         https://unsplash.com/
+Halikei | 기하학적 배경화면                          https://app.haikei.app/
+Shape Divider App | 기하학적 배경화면2               https://www.shapedivider.app/
+cool Backgrounds | 심풀한 배경이미지                 https://coolbackgrounds.io/
+PNGEgg | PNG 파일 모움                               https://www.pngegg.com/
+구글 폰트, 머티리얼 아이콘                            https://fonts.google.com/icons
+부트스트랩                                          https://getbootstrap.kr/docs/5.1/getting-started/introduction/
+폰트어썸아이콘 1                                     https://fontawesome.com/icons
+폰트어썸아이큰 2                                     https://fontawesomeicons.com/send-o
+Tailwind CSS | Docs                                 https://tailwindcss.com/docs/installation
+Talilwind Css Alerts | Tailwind 컴포년트 모음        https://tailwindcomponents.com/components/alerts
+Tailwind U KIT | Talwind 컴포넌트 모음2              https://www.tailwind-kit.com/components
+Flowbite | Tailwind 컵포년트 모음3                   https://flowbite.com/docs/getting-started/introduction/#figma
+Framer Motion | 애니메이션 라이브러리                 https://www.framer.com/motion/
+Animate.css | 애니메이션 효과 모음 라이브러리          https://animate.style/
+Animista | 애니메이선 효과 모읍                       https://animista.net/play/text/tracking-in
+Hover.css | 하버효과 모음                            http://ianlunn.github.io/Hover/
+css Grid Generator| 그리드 추출                     https://cssgrid-generator.netlify.app/?fbclid=IwAR2xX6U6DfhzDHmjXMiyBeiYhXr9m094SKHrIOYHu3zghabCudQpb4cwSFc
+css Gradient | 그라데이션 효과 추출                   https://cssgradient.io/
+colorspace | 어울리는 컬러                             https://mycolor.space/
+My Dashboard | 각종 Converter 및 SVG 만들기            https://www.fetoolkit.io/dashboard/MmFkLTIwNA
+Greensock (GSAD) | 얘니메이션 라이브러리               https://greensock.com/st-demos/
+LottieFles | 다양한 GIF 그림 다운                      https://lottiefiles.com/
+sketchfab | 다양한 3D 파일 다운                        https://sketchfab.com/search?type=models
+Creators 3D | 3D 컨버터                                https://www.creators3d.com/online-viewer
+Metaverse 3D Avatar Creator | 아바타 만들기             https://readyplayer.me/
+Free profile picture Maker | 프로필 사진 배경 만들기    https://pfpmaker.com/
+cubic-bezier | transition 속력 확인                    https://cubic-bezier.com/#.17,.67,.83,.67
+버튼 디자인 스타일 모음                                 https://inpa.tistory.com/entry/CSS-%F0%9F%92%8D-%EB%B2%84%ED%8A%BC-%EB%94%94%EC%9E%90%EC%9D%B8-%EB%AA%A8%EC%9D%8C
+Loaders, Spinners 모음                                 https://cssloaders.github.io/
+Css 매니메이션 18가지 예제                              https://ldrerin.tistory.com/397
 
+
+######## 그외 프론트엔드 관련
+
+코드펜                   https://codepen.io/pen/?editors=0012
+코드 샌드박스            https://codesandbox.io/dashboard/home?workspace=9fbdea18-e125-4d0e-8b8f-9a1631cbf2d6
+타입스크립트 가상연습     https://www.typescriptlang.org/play?#
+리액트 DOCS              https://ko.reactjs.org/docs/getting-started.html
+Create-react-app DOCS    https://create-react-app.dev/docs/getting-started
+Nextjs DOCS              https://beta.nextjs.org/docs/api-reference/components/image
+Nextjs13 DOCS            https://nextjs.org/docs/upgrading
+Next Auth DOCS           https://next-auth.js.org/
+리액트 아이콘             https://react-icons.github.io/react-icons/
+리액트 쿼리 사용법         https://velog.io/@dkdlel102/React-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EC%BF%BC%EB%A6%AC-useQuery-%EC%82%AC%EC%9A%A9%EB%B2%95
+w3schools 튜토리얼        https://www.w3schools.com/default.asp
+MDN 튜토리얼              https://developer.mozilla.org/ko/docs/Web
+핸드폰 사이즈 (반응형)     https://experienceleague.adobe.com/docs/target/using/experiences/vec/mobile-viewports.html
+HTML기호코드              https://www.toptal.com/designers/htmlarrows/
 
 ######## 백엔드, API, 배포관련
-PvthonAnwwhere https://www.pythonanywhere.com/
-Netlify https://www.netlify.com/
-Vercel https://vercel.com/dashboard
-클라우드타입 https://cloudtype.io/
-PvthonAnwwhere 세팅 관련 모음 https://gaussian37.github.io/python-etc-pythonanywhere-setting/
-단계별 가이드: Netlify의 cactus https://www.netlify.com/blog/2016/04/08/a-step-by-step-guide-cactus-on-netlify/
-Firebase | serverless DB https://firebase.google.com/docs/reference/js/firestore_?authuser=0
-Upstash | Serverless DB https://upstash.com/
-Pusher1 | 실시간 기능 https://pusher.com/
-RapidAPI | 각종 API 모음 https://rapidapi.com/ytdlfree/api/youtube-v31/
-공공API | 각종 공공 API 모음2 https://github.com/public-apis/public-apis
-공공데이터프털 | 각종 한국 공공 API 모음 https://www.data.go.kr/
+PvthonAnwwhere                          https://www.pythonanywhere.com/
+Netlify                                 https://www.netlify.com/
+Vercel                                  https://vercel.com/dashboard
+클라우드타입                             https://cloudtype.io/
+PvthonAnwwhere 세팅 관련 모음            https://gaussian37.github.io/python-etc-pythonanywhere-setting/
+단계별 가이드: Netlify의 cactus          https://www.netlify.com/blog/2016/04/08/a-step-by-step-guide-cactus-on-netlify/
+Firebase | serverless DB                https://firebase.google.com/docs/reference/js/firestore_?authuser=0
+Upstash | Serverless DB                 https://upstash.com/
+Pusher1 | 실시간 기능                    https://pusher.com/
+RapidAPI | 각종 API 모음                 https://rapidapi.com/ytdlfree/api/youtube-v31/
+공공API | 각종 공공 API 모음2             https://github.com/public-apis/public-apis
+공공데이터프털 | 각종 한국 공공 API 모음   https://www.data.go.kr/
