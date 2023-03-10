acf96ddba78ddf21c563dba8d1cde02f2aeadd3e
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-03-10 10:03:02 -0600

    Update React_Testing_Jest.js

--- React_Testing_Jest.js
+++ React_Testing_Jest.js
@@ -33,7 +33,7 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 // Jest -> 단언(Assertion)인 expect, test 등 과 같이 테스트를 직접 하게함    // Jest Watch Mode는 코밋할때 변화가 있으면 실행 (변화없으면 실행X)
   
 // ####### 테스트의 종류
-//  단위 테스트 (Unit test)
+//  단위 테스트 (Unit test)  : 로직이 너무 복잡한경우, 코드 품질을 개선하기 위해(안정성, 완성도), 버그발생 최소화, 문서화, 오류 조기발견, 코드변경 용이
 //  통합 테스트 (Integration test)
 //  기능 테스트 (Functional test)
 //  인수 테스트 (Acceptance test / End-t-End Test / E2E Test)
@@ -41,7 +41,7 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 // #################################################################################################################################################
 // ####### Color Button App ########################################################################################################################
 // #################################################################################################################################################
-// ###### 빨간, 파랑 토글 버튼과 체크박스 테스트 ####################################################################################################################
+// ###### 빨간, 파랑 토글 버튼과 체크박스 테스트 #######################################################################################################
 // ####### App.test.js (create-react-app 기준)
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
@@ -51,9 +51,9 @@ test('Button has correct initial color, and updates when clicked', () => {
   const colorBtn = screen.getByRole("button", { name: "Change to blue" }); // "Change to blue" 라는 버튼이 있는지
   expect(colorBtn).toHaveStyle(`background-color: red`)
 
-  fireEvent.click(colorBtn); // 버튼 클릭 발생시
-  expect(colorBtn).toHaveStyle(`background-color: blue`) // 파랑색으로 변경
-  expect(colorBtn).toHaveTextContent('Change to red')
+  fireEvent.click(colorBtn); // 버튼 클릭 발생시에
+  expect(colorBtn).toHaveStyle(`background-color: blue`) // 파랑색으로 변경되있는지
+  expect(colorBtn).toHaveTextContent('Change to red') // 해당 문자가 있는지
 });
 
 test('Initial conditions',() => {
@@ -98,10 +98,30 @@ test('Button has correct initial color', () => {
   expect(colorBtn).toHaveStyle(` background-color: red `)
 });
 
+// ###### 유닛 테스팅 함수 ####################################################################################################################################
+//  describe는 테스트를 그룹으로 묶어줌
+// ####### App.test.js (create-react-app 기준)
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+import { replaceCamelWithSpaces } from './App';
+
+// ... 생략 ...
+
+describe('spaces before came-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue'); // 함수 인자와 리턴값 (글자사이에 대문자가 있으면 한칸띄워서 나오는지 테스트)
+  });
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
+})
+
+// ####### App.js (create-react-app 기준)
+export const replaceCamelWithSpaces = (colorName) => {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');  // 정규표현식 (글자사이에 대문자가 있으면 앞에 한칸씩 띄어주는역할)
+}
+
 // ######  ####################################################################################################################################
-
-
-
-
-
-
