dbc8a2a817be4aa904c34369ed02e812d78fd1f3
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-03-10 18:43:08 -0600

    Update React_Testing_Jest.js

--- React_Testing_Jest.js
+++ React_Testing_Jest.js
@@ -50,10 +50,10 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 
 // getBy...	      Throw error	      Return element	      Throw error	          No
 // queryBy...  	  Return null     	Return element	      Throw error	          No
-// findBy...	      Throw error     	Return element	      Throw error	          Yes
+// findBy...	    Throw error     	Return element	      Throw error	          Yes
 
 // getAllBy... 	  Throw error	        Return array	      Return array	        No
-// queryAllBy...	  Return []	          Return array	      Return array     	    No
+// queryAllBy...  Return []	          Return array	      Return array     	    No
 // findAllBy...	  Throw error       	Return array	      Return array	        Yes
 // -------------------------------------
 
