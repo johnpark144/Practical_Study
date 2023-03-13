6fda12a6f97a11f38c6344d4e411b1fcaf91d000
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-03-13 12:06:48 -0500

    Update React_Testing_Jest.js

--- React_Testing_Jest.js
+++ React_Testing_Jest.js
@@ -1,8 +1,8 @@
-// ####### 리미인드 ###########################################################################################################################################
+// ################ 리미인드 ###################################################################################################################################
 // 다하고 이론부분 지을거 지우기
 // 매쳐(Matcher) 효과 정리하기
 
-// ####### RTL 세팅 및 간단한 이론  ########################################################################################################################
+// ################ RTL 세팅 및 간단한 이론  ##################################################################################################################
 // TDD는 항상 기능을 추가하기전 먼저 테스트코드를 작성하여야한다
 // 테스팅 라이브러리 Docs : https://testing-library.com/docs/
 
@@ -10,22 +10,22 @@
 // getByRole의 Role부분 정리 :  https://www.w3.org/TR/wai-aria/#textbox
 // 매쳐(Matcher) 정리: https://github.com/testing-library/jest-dom#tohavetextcontent
 
-// ####### RTL과 Jest의 역할
+// ################ RTL과 Jest의 역할
 // RTL -> 가상 Dom을 만들거나 상호작용하는 등 테스트를 간접적으로 돕는 역할 (render, screen, fireEvent 등)
 // Jest -> 단언(Assertion)인 expect, test 등 과 같이 테스트를 직접 하게함    // Jest Watch Mode는 코밋할때 변화가 있으면 실행 (변화없으면 실행X)
   
-// ####### 테스트의 종류
+// ################ 테스트의 종류
 //  단위 테스트 (Unit test)  : 로직이 너무 복잡한경우, 코드 품질을 개선하기 위해(안정성, 완성도), 버그발생 최소화, 문서화, 오류 조기발견, 코드변경 용이
 //  통합 테스트 (Integration test)
 //  기능 테스트 (Functional test)
 //  인수 테스트 (Acceptance test / End-t-End Test / E2E Test)
 
-// ####### package.json (create-react-app 기준)
+// ################ package.json (create-react-app 기준)
 "scripts": {
     "test": "react-scripts test",
   },
     
-// ####### App.test.js (create-react-app 기준)
+// ################ App.test.js (create-react-app 기준)
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -36,14 +36,15 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 });
 
 
-// ####### 단언(Assertion)인 expect과 매쳐(Matcher)인 . 
+// ################ 단언(Assertion)인 expect과 매쳐(Matcher)인 . 
   expect(element.textContent).toBe('hello');  // expect안에 있는게 'hello'여야함
   expect(elementsArr).toHaveLength(7);  // 배열의 길이가 7이어야함
 
-// ######## screen 메소드 ##############################################################################################################################
+// ################ screen 메소드 ####################################################################################################################
 // getBy, queryBy, findBy는 컴포넌트의 요소들을 확인하는 데 사용
 // getBy, queryBy, findBy 정리 : https://testing-library.com/docs/react-testing-library/cheatsheet/
-// getBy > queryBy > findBy (사용 추천 순) // getBy 중에서도 getByRole이 가장 선호됨
+// getBy > queryBy > findBy (사용 추천 순)  // queryBy는 주로 없는게 정상인 태그를 확인할때 // findBy는 주로 비동기 데이터를 불러올때 사용
+// getBy 중에서도 getByRole이 가장 선호됨.
 
 // ------------------------------------
 // Type of Query 	0 Matches	            1 Match	          >1 Matches	    Retry(Async/Await)
@@ -57,11 +58,11 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 // findAllBy...	  Throw error       	Return array	      Return array	        Yes
 // -------------------------------------
 
-// ###### ESLint, Prettier ###########################################################################################################################
+// ################ ESLint, Prettier ###############################################################################################################
 // ESLint : 린터(Linter)로 사용되는 툴 중 하나, 소프트웨어 코드를 분석하여 버그를 찾고, 코드 스타일이나 실수를 검사하는데 사용. 문법 오류, 디버깅 및 성능 최적화 실수, 가독성 및 일관성 오류 등을 찾아냄
 // Prettier : 포매터(Formatter)로써, 소프트웨어 코드의 모양을 바꾸는 툴로, 들여쓰기나 공백, 줄바꿈 등의 공통적인 코딩 스타일을 적용하기 위해 사용
 
-// ###### 테스팅라이브러리와 jest-dom를 위한 ESLint #############################################################################################
+// ################ 테스팅라이브러리와 jest-dom를 위한 ESLint #############################################################################################
 // npm i eslint-plugin-testing-library eslint-plugin-jest-dom
 
 // 규칙 참고 링크
@@ -69,7 +70,7 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 // https://github.com/testing-library/eslint-plugin-testing-library
 // https://github.com/bonnie/bonniedotdev/blob/main/client/.eslintrc.json
 
-// ###### package.json 파일에서 해당내용 지우기
+// ################ package.json 파일에서 해당내용 지우기
 "eslintConfig": {
     "extends": [
       "react-app",
@@ -77,7 +78,7 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
     ]
   },
 
-// ###### .eslintrc.json (규칙을 정할 수 있음, 위에 규칙링크 참고)
+// ################ .eslintrc.json (규칙을 정할 수 있음, 위에 규칙링크 참고)
 {
   "plugins": ["testing-library", "jest-dom"],
   "extends": [
@@ -87,7 +88,7 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
     "plugin:jest-dom/recommended"
   ]
 }
-// ###### .vscode/settings.json (prettier, eslint 같이)
+// ################ .vscode/settings.json (prettier, eslint 같이)
 {
   "editor.codeActionsOnSave": { "source.fixAll.eslint": true },
   "editor.defaultFormatter": "esbenp.prettier-vscode",
@@ -99,8 +100,8 @@ test('renders learn react link', () => {  // global test 메서드는 두 인자
 // #################################################################################################################################################
 // ####### Color Button App ########################################################################################################################
 // #################################################################################################################################################
-// ###### 빨간, 파랑 토글 버튼과 체크박스 테스트 (fireEvent, getByRole) ################################################################################
-// ####### App.test.js (create-react-app 기준)
+// ################ 빨간, 파랑 토글 버튼과 체크박스 테스트 (fireEvent, getByRole) ###################################################################
+// ################ App.test.js (create-react-app 기준)
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
@@ -123,7 +124,7 @@ test('Initial conditions',() => {
   expect(checkbox).not.toBeChecked(); // checkbox가 체크가 안된 상태인
 })
 
-// ####### App.js (create-react-app 기준)
+// ################ App.js (create-react-app 기준)
 import { useState } from 'react';
 import './App.css';
 
@@ -141,10 +142,10 @@ function App() {
 }
 export default App;
 
-// ###### LogRoles ####################################################################################################################################
+// ################ LogRoles ############################################################################################################################
 // 페이지가 길어서 역할이있는 항목들이 햇갈릴때 사용됨 (역할과 이름이 콘솔로그 처럼 출력됨) // 코드가 지저분해질 가능성이 있어서 안쓰는 경우도 많음
 
-// ####### App.test.js (create-react-app 기준)
+// ################ App.test.js (create-react-app 기준)
 import { render, screen } from '@testing-library/react';
 import { logRoles } from '@testing-library/dom'
 import App from './App';
@@ -157,9 +158,9 @@ test('Button has correct initial color', () => {
   expect(colorBtn).toHaveStyle(` background-color: red `)
 });
 
-// ###### 그룹 테스트, 유닛 테스팅 함수 ###########################################################################################################################
+// ################ 그룹 테스트, 유닛 테스팅 함수 ##############################################################################################################
 //  describe는 테스트를 그룹으로 묶어줌
-// ####### App.test.js (create-react-app 기준)
+// ################ App.test.js (create-react-app 기준)
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import { replaceCamelWithSpaces } from './App';
@@ -187,7 +188,7 @@ export const replaceCamelWithSpaces = (colorName) => {
 // #################################################################################################################################################
 // ####### 아이스크림 주문 앱 ########################################################################################################################
 // #################################################################################################################################################
-// ######## fireEvent와 userEvent ##################################################################################################################
+// ################ fireEvent와 userEvent ######################################################################################################
 // fireEvent : userEvent보다 정교하지는 않지만, 작성하기 쉽고 이용할 옵션이 많음
 
 // userEvent :
@@ -196,8 +197,8 @@ export const replaceCamelWithSpaces = (colorName) => {
 
 // 결론 : userEvent에 있는것은 userEvent에서쓰고 없으면 fireEvent에서 사용할 것을 권장.
 
-// ######## userEvent (실제 시뮬레이션) #######################################################################################################################
-// ######## SummaryForm.test.js
+// ################ userEvent (실제 시뮬레이션) ###########################################################################################################
+// ################ SummaryForm.test.js
 import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
@@ -217,7 +218,7 @@ test("Checkbox enables button on first click and disables on second click", asyn
 });
 
 
-// ######## SummaryForm.js
+// ################ SummaryForm.js
 import React, { useState } from "react";
 
 function SummaryForm() {
@@ -232,8 +233,8 @@ function SummaryForm() {
 export default SummaryForm;
 
 
-// ######## queryBy (매치되는 태그가 없으면 null을 반환) 와 hover ###################################################################################################
-// ######## SummaryForm.test.js
+// ################ queryBy (매치되는 태그가 없으면 null을 반환) 와 hover #########################################################################################
+// ################ SummaryForm.test.js
 import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
@@ -260,7 +261,7 @@ test("Popover responds to hover", async () => {
 });
 
 
-// ######## SummaryForm.jsx
+// ################ SummaryForm.jsx
 import React, { useState } from "react";
 
 function SummaryForm() {
@@ -284,21 +285,33 @@ function SummaryForm() {
 export default SummaryForm;
 
 
-// ######## Mock Service Worker (MSW) ############################################################################################################
+// ################ Mock Service Worker (MSW) - 요청 가로채기 ################################################################################################
 // npm install msw
 // https://mswjs.io/docs/
 
-// ######## mocks/server.js
+// ################ mocks/server.js
 import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
 // This configures a request mocking server with the given request handlers.
 export const server = setupServer(...handlers);
 
-// ######## mocks/handlers.js
+// ################ setupTests.js (App.js와 같은폴더)
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server.js";
+
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+// ################ mocks/handlers.js
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("http://localhost:3000/scoops", (req, res, ctx) => {
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => { // http://localhost:3030/scoops 로 get할시 서버없이 요청을 가로채서 밑에 json파일을 제공
     return res(
       ctx.json([
         { name: "Chocolate", imagePath: "/images/chocolate.png" },
@@ -308,24 +321,80 @@ export const handlers = [
   }),
 ];
 
-// ######## setupTests.js (App.js와 같은폴더)
-import "@testing-library/jest-dom";
-import { server } from "./mocks/server.js";
 
-// Establish API mocking before all tests.
-beforeAll(() => server.listen());
+// ################################################################################################## await findBy // with Axios ########################
+// npm install axios
 
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests.
-afterEach(() => server.resetHandlers());
+// ################ entry/test/options.test.js
+import { render, screen } from "@testing-library/react";
+import Options from "../Options";
 
-// Clean up after the tests are finished.
-afterAll(() => server.close());
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i }); // findBy는 주로 비동기 데이터를 불러올때 사용
+  expect(scoopImages).toHaveLength(2);
+
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]); // toEqual은 altText 배열과 동일한 값을 가졌는지 확인
+});
 
 
+// ################ entry/options.jsx
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import axios from "axios";
 
-// ########  ############################################################################################################
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {});
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+  return <div>{optionItems}</div>;
+
+// ################ entry/ScoopOption.jsx
+export default function ScoopOption({ name, imagePath }) {
+  return (
+    <div>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} scoop`}
+      />
+    </div>
+  );
+}
+
+// ################ entry/ToppingOption.jsx
+export default function ToppingOption({ name, imagePath }) {
+  return (
+    <div>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:3030/${imagePath}`}
+        alt={`${name} topping`}
+      />
+    </div>
+  );
+}
+
+
+// ##########  #########################################################################################################################
 
 
 
