73d7e87ddae18221cc9a2a978585854e23f54096
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-04-12 15:44:15 +0900

    Update React.js

--- React.js
+++ React.js
@@ -34,7 +34,7 @@
 // Link 특정 앵커로 보내기 -- HashLink, NavHashLink
 // Link 이동할때 특정 state같이 보내기 -- useLocation
 // React.lazy()와 Suspense  -- 컴포넌트의 로딩 시점을 지연, 대체정보를 출력
-// useTransition, useDeferredValue -- 
+// useTransition, useDeferredValue -- 동시 처리되어 버벅거릴 때, UX을 개선, 늦게처리함
 
 // ######### 리액트 관한 정보 링크 #######################################################################################################################
 // 폴더 구조 : https://velog.io/@raverana96/react-%EB%A6%AC%EC%95%A1%ED%8A%B8-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EC%9D%98-%ED%8F%B4%EB%8D%94%EA%B5%AC%EC%A1%B0
@@ -1293,7 +1293,7 @@ function App() {
 }
 
 // ####### useTransition, useDeferredValue #######################################################################################################################
-// 비동기적인 작업이 수행되어 여러가지 작업이 동시 처리되어 버벅거릴 때 잠시 화면상의 변화가 일어나지 않도록 설정할 수 있습니다. 이를 통해 사용자 경험을 개선할 수 있습니다
+// 비동기적인 작업이 수행되어 여러가지 작업이 동시 처리되어 버벅거릴 때 잠시 화면상의 변화가 일어나지 않도록 설정할 수 있습니다. 이를 통해 UX을 개선할 수 있습니다
 
 // ####### useTransition 예시
 import { useTransition, useState } from "react";
