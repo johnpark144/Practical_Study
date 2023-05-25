b12a5256065ab9adeb7f60d6d59eb0b5ba92a70b
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-05-25 16:46:14 +0900

    Update Nextjs_MongoDB.js

--- Nextjs_MongoDB.js
+++ Nextjs_MongoDB.js
@@ -1,4 +1,14 @@
 // ######### 기본 #######################################################################################################################################
 // npm i bcrypt mongodb mongoose next-auth    // bcrypt는 비밀번호 해시화
 
+// 구글 API : console.cloud.google.com/
 
+// 몽고디비 : 
+프로젝트 생성 -> Connect -> drivers -> MONGODB_URI 코드를 가져옴
+MONGODB_URI=mongodb+srv://vyckd354:<password>@cluster0.gctypic.mongodb.net/?retryWrites=true&w=majority
+
+password 부분은 Database access부분에 비밀번호를 대체해서 넣기. 이와같이 -> mongodb+srv://vyckd354:O4Ivm4D3FSOTMnKi@cluster0.gctypic.mongodb.net/?retryWrites=true&w=majority
+
+Network Access에서 내 ip주소 접근 가능하게 혹은 "0.0.0.0/0" 를 적어줌으로써 모든 ip에서 접근 가능하게
+
+// ####################################################################################################################################################
