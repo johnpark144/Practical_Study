e7c08c63e7f4d43a3811690e186f9546d62ddcd6
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-06-17 01:33:07 +0900

    Update Nextjs_MongoDB_NextAuth.js

--- Nextjs_MongoDB_NextAuth.js
+++ Nextjs_MongoDB_NextAuth.js
@@ -101,7 +101,7 @@ export const connectToDB = async () => {
   try {
     // mongoDB 연결시킴
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share_prompt",
+      dbName: "share_prompt",   // 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
