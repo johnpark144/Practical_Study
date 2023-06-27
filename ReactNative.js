12b38f086be41598479b3a39c9c411c87781177f
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-06-27 18:31:11 +0900

    Update ReactNative.js

--- ReactNative.js
+++ ReactNative.js
@@ -8,19 +8,24 @@
 
 // ######## 기본 참고 사이트 ############################################################################################################################
 //  https://reactnative.dev/docs/intro-react-native-components  // 리액트 네이티브 핵심 컴포넌트
+// https://icons.expo.fyi     // 아이콘 사이트
 
-// ######## 기본 세팅 ############################################################################################################################
+
+
+// ######## Expo 기본 세팅 ############################################################################################################################
 // npm i -g expo-cli   // Expo를 사용 가능하게
 // npx create-expo-app@latest -e with-router  // Expo로 리액트 네이티브 앱 만들기
 
 
 // ################################################################################################## eslint, prettier ############################
+// https://bobbyhadz.com/blog/replace-double-quotes-with-single-in-vscode    // eslint, prettier 오류 시 참고
+
 // ############## eslint 설정
 // npm i eslint --save-dev  // eslint 다운
 // npx eslint --init  // eslint 초기설정
 // npm i @react-native-community/eslint-config --save-dev
 
-// ############## .eslintrc.js
+// ############## .eslintrc.js (나중에 변경)
 module.exports = {
   env: {
     browser: true,
@@ -53,7 +58,7 @@ module.exports = {
 // .prettierrc.js 파일 생성
 // package.json 안에 scripts에 "lint": "eslint ." 입력  // cli로 전체 문법 검사
 
-// ############## .prettierrc.js
+// ############## .prettierrc.js (나중에 변경)
 module.exports = {
   bracketSpacing: true,
   singleQuote: true,
@@ -65,7 +70,10 @@ module.exports = {
 
 // ######## 기본 컴포넌트 설명 ############################################################################################################################
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native"; \
+// View = Div, Text = P, 
+// SafeAreaView 는 탐색 표시줄, 탭 표시줄, 툴바, 기타 상위 뷰에서 다루지 않는 뷰 부분을 반영하도록 패딩을 자동 적용시켜줌
+// StyleSheet는 Css효과를 줄 수있는 객체
 
 const App = () => {
   return (
@@ -103,7 +111,12 @@ const styles = StyleSheet.create({
 
 export default App;
 
+// ######## 아이콘 ############################################################################################################################
+// https://icons.expo.fyi     // 아이콘 검색하여 사용하게 함
+import { Feather } from "@expo/vector-icons";
+<Feather name="sun" size={100} color="black" />
 
-
+// ######## 아이콘 ############################################################################################################################
+          
 // @@@@@@@@@@@ Weather App @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // ######## ############################################################################################################################
