0c0f63069dd7b9a5f39390c930319fdc66d17b74
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-07-03 15:35:01 +0900

    Update ReactNative.js

--- ReactNative.js
+++ ReactNative.js
@@ -1258,7 +1258,6 @@ const handlePagination = (direction) => {
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
 
-// Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context("./app");
   return <ExpoRoot context={ctx} />;
