87c88e570784802dfed5d457fc5f6503f4263d03
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-07-18 06:37:31 +0900

    Update ReactNative.js

--- ReactNative.js
+++ ReactNative.js
@@ -1,11 +1,11 @@
 // ######### 리마인더 #######################################################################################################################################
 // SplashScreen 정리하기 --> SplashScreen.preventAutoHideAsync()는 SplashScreen.hideAsync()가 뜰떄까지 임시화면을 뿌려줌(브랜드 로고 등)
 // 사진 기능
-// 위치정보 기능
 // 알림 기능
 // flex: 1 에 대해
 // 컴퓨터로 화면 확인법
 // keyboardAvoidingView(키보드쳐야할때 View스타일 바꿀 수 있음, Platform(안드인지 아이폰인지)
+// tailwind 사용 하려면 nativeWind 사용
 
 
 // ######### 인덱스 (Ctrl + F) ########################################################### (-> 인덱스에 있는데 찾기 안되면 찾아서 인덱스 변경) ##################
@@ -79,6 +79,9 @@ module.exports = {
   "singleQuote": true,
   "trailingComma": "all"
 }
+
+// ########  ##################################################################################################################################
+// ################ 
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // @@@@@@@@@@@ Weather App @@@@@@@@ (React Navigation 이용) @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -1762,7 +1765,6 @@ const styles = StyleSheet.create({
 // ######## Map ######################################################################################################################################################
 // expo install react-native-maps
 
-
 // ################ 여기선 구현하지 않은 참고 사항
 // 지도 주소 찾기 기능은 googlePlacesAutocpmplete 사용
 // 지도 길찾기 기능은 react-native-maps-directions을 사용
@@ -1796,3 +1798,160 @@ const DeliveryScreen = () => {
 };
 
 export default DeliveryScreen;
+
+// ###############################################################################################################################
+// ########## 기타 ReactNative 유용한것들 ###########################################################################################
+// ###############################################################################################################################
+// ######## 카메라 기능 ###############################################################################################################################
+// expo install expo-media-library
+// expo install expo-camera
+
+// ################ 
+import { Camera, CameraType } from 'expo-camera';
+import { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { Entypo } from '@expo/vector-icons';
+
+const CameraScreen = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+
+  const cameraRef = useRef(null);
+
+  // 카메라 허용 여부
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync(); // 카메라 여부 창 띄우기
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted'); // 카메라 허용
+    })();
+  }, []);
+
+  // 카메라 허용하지 않은경우
+  if (hasCameraPermission === false) return <Text>No access to camera</Text>;
+
+  // 사진 찍을 때
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync(); // 사진 데이터
+        setImage(data.uri);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  // 사진 저장
+  const saveImage = async () => {
+    if (image) {
+      try {
+        await MediaLibrary.createAssetAsync(image);
+        alert('picture save!');
+        setImage(null);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  // 버튼 생성  // 파라미터 : (아이콘, 클릭한경우 실행함수, 글씨, 색깔(디폴트: 하얀색))
+  const Button = (icon, onPress, text, color = 'white') => {
+    return (
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Entypo name={icon} size={28} color={color} />
+        <Text style={styles.text}>{text}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {!image ? ( // 아직 찍은 사진이 없는 경우, 카메라 작동
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+        >
+          <View style={styles.cameraOption}>
+            {/* 전면, 후면 카메라 여부 */}
+            {Button('retweet', () => {
+              setType(
+                type === CameraType.back ? CameraType.front : CameraType.back
+              );
+            })}
+            {/* 플래시 여부 */}
+            {Button(
+              'flash',
+              () => {
+                setFlash(
+                  flash === Camera.Constants.FlashMode.off
+                    ? Camera.Constants.FlashMode.on
+                    : Camera.Constants.FlashMode.off
+                );
+              },
+              null,
+              flash === Camera.Constants.FlashMode.on ? 'white' : 'yellow'
+            )}
+          </View>
+        </Camera>
+      ) : (
+        // 찍은 이미지
+        <Image source={{ uri: image }} style={styles.camera} />
+      )}
+      <View>
+        {image ? ( // 사진찍은 후 다시찍을지, 저장할지 여부 버튼
+          <View style={styles.afterSnap}>
+            {Button('retweet', () => setImage(null), 'Re-take')}
+            {Button('save', saveImage, 'Save')}
+          </View>
+        ) : (
+          // 사진찍기 버튼
+          Button('camera', takePicture, 'Take a picture')
+        )}
+      </View>
+    </View>
+  );
+};
+export default CameraScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    paddingBottom: 20,
+  },
+  camera: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  afterSnap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 50,
+  },
+  button: {
+    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#F1F1F1',
+    marginLeft: 10,
+  },
+  cameraOption: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 30,
+  },
+});
+
