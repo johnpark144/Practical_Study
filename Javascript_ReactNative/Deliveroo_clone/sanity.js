b1e566bf751771d476178aaa7128b74ba60ba55d
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-07-19 05:18:45 +0900

    Add files via upload

---
+++ Javascript_ReactNative/Deliveroo_clone/sanity.js
@@ -0,0 +1,19 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import 'react-native-url-polyfill/auto';
+
+const client = createClient({
+  projectId: 'zpj6qd2o', // sanity.config.ts 혹은 sanity사이트에있음
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2021-10-21',
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+
+export default client;
+
+// http://localhost:19000이랑 http://localhost:3000를 CORS origins 리스트에 적어두기
+// 방법 1 ) https://www.sanity.io/manage/personal/project/(프로젝트ID)/api에 CORS origins 적기
+// 방법 2 ) cd sanity --> sanity cors add http://localhost:19000
