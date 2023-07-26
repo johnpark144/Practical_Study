77e54acb5ee0b149d6edfa5fa3fa40445de56cc1
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-07-26 14:14:37 +0900

    Update ReactNative.js

--- ReactNative.js
+++ ReactNative.js
@@ -1,17 +1,15 @@
 // ######### 리마인더 #######################################################################################################################################
 // expo-auth
-// https://www.youtube.com/watch?v=Ud_GWxu1_Xg&t=185s  // 요거 코드보고 정리해보기 (auth)
 
-// 달력
 // mongoDB같이쓰기
 // Realm (로컬 데이터베이스로 사용,  데이터를 저장, 검색, 쿼리 및 동기화하는 기능을 제공)
 // 인덱스 적고 프로젝트 만들어 보기
 
 // Vibration(바이브레이션) : https://reactnative.dev/docs/vibration  (이처럼 자주쓰일것 같은 것들은 리액트네이티브, 엑스포 docs 링크 걸어두고 나중에 사용할떄 정리)
-
 // alert
 // 얼굴 인식
 // 알림 기능
+// 달력
 
 // expo라우팅 폴더 예약어 
 // Link
@@ -2301,13 +2299,6 @@ export default function App() {
     return (
       <Animated.View
         style={{
-          flexDirection: 'row',
-          width: wp(90),
-          height: hp(10),
-          borderRadius: 10,
-          backgroundColor: '#ddd',
-          marginHorizontal: wp(5),
-          marginBottom: hp(1),
           opacity: opacity, // 스크롤에 따른 선명도 애니메이션
           transform: [
             // 스크롤에 따른 크기 애니메이션
